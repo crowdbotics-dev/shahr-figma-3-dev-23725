eaec2d6f95b0cbada8ef53925ba9e106c140edd6
Crowdbotics <engineering@crowdbotics.com>
2022-03-17 19:34:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shahr_figma_3_dev_23725/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shahr_figma_3_dev_23725;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shahr_figma_3_dev_23725";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shahr_figma_3_dev_23725",
+  url: "https://shahr_figma_3_dev_23725.botics.co",
+  api: "https://shahr_figma_3_dev_23725.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
